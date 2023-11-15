31f419753692daadc52a18c038b184fb53180e3a
dairyssb <dairyssb@gmail.com>
2023-11-14 22:10:30 -0600

    finished first and initial attempt on 2nd

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let stringHolder = string;
+    for(let i = 0; i < num; i++){
+        let result =+ stringHolder;
+    }
+    return result;
 };
 
 // Do not edit below this line
