e473b4ecaf5f2ea36b9b4ed9b4b2a77e76502bf7
dairyssb <dairyssb@gmail.com>
2023-11-14 22:45:54 -0600

    third one is complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    //initalizing result and placeholder to capture last string.
+    let result = "";
+    let placeholder = "";
+    
+    //looping around string to capture last char and reassign to result
+    for(let i = 1; i <= string.length; i++){
+        placeholder = string.substr(string.length - i, 1);
+        result += placeholder;
+        //placeholder = "";
+    }
+    return result;
 };
 
 // Do not edit below this line
