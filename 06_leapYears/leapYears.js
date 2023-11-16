66084b61e56cca25829455e44004314808197769
dairyssb <dairyssb@gmail.com>
2023-11-15 20:51:45 -0600

    finished 6th

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    let answer = true;
+    if(year % 4 == 0){
+        if(year % 100 != 0 && year % 400){
+            answer = true;
+        }
+        //this is for century years that fit the rules
+        else if (year % 100 == 0 && year % 400) {
+            answer = false;
+        }
+    }
+    else {
+        answer = false;
+    }
+    return answer;
 };
 
 // Do not edit below this line
