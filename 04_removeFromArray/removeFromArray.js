d32899d6f6654cf2958fe484a3dde4a21411d1de
dairyssb <dairyssb@gmail.com>
2023-11-15 19:34:39 -0600

    4th attempt completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...theArgs) {
+    let newArray = [];
+    //loop through array to find if the values are included in the array
+    for(let i = 0; i < array.length; i++){
+        if(!theArgs.includes(array[i])){
+            //pushing values that are not args to the new array
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
