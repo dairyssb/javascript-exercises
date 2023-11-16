8774d6668302a4f63a04ee822901d45ab8f60965
dairyssb <dairyssb@gmail.com>
2023-11-15 20:13:14 -0600

    5th one is complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let finalSum = 0;
+        if(num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+            return "ERROR";
+        }
+        else if(num1 < num2){
+            for(; num1 <= num2; num1++){
+                finalSum += num1;
+            }
+        }
+        else if (num1 > num2){
+            for(; num2 <= num1; num2++){
+                finalSum += num2;
+            }
+        }
+    return finalSum;
 };
 
 // Do not edit below this line
