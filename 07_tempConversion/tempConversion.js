f643ba2f4f5f74025eb2e1dc1187211994c49f05
dairyssb <dairyssb@gmail.com>
2023-11-15 21:07:00 -0600

    7th completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  //C = (F - 32) * 5/9
+  let rounded = Math.round(((temp - 32) * (5/9)) * 10) / 10;
+  let fixed = rounded.toFixed(1);
+  return rounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  //F = (C * 9/5) + 32
+  let rounded = Math.round(((temp * 9/5) + 32) * 10) / 10;
+  let fixed = rounded.toFixed(1);
+  return rounded
 };
 
 // Do not edit below this line
